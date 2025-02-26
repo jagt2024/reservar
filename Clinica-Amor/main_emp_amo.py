26bad687110d2ca58a276be654c0f3ea3a9ec1c6
jagt2024 <josea_garciat@hotmail.com>
2025-02-26 17:14:36 -0500

    update 37

--- Clinica-Amor/main_emp_amo.py
+++ Clinica-Amor/main_emp_amo.py
@@ -125,9 +125,9 @@ class Model:
   menuTitle = "Programe ya sus Solicitudes"
   option1 = 'Inicio'
   #option10 = 'Descargar Agenda'
-  option9  = 'Consultar Agenda'
+  option9  = 'Consultar Agenda Citas'
   option2  = 'Control Asistencias'
-  option4  = 'Iiniciar Recordatorio'
+  option4  = 'Iiniciar Recordatorio Citas'
   option3  = 'Copia de Seguridad'
   #option22 = 'Consulta Pagos'
   #option5  = 'Nuestros Servicios'
