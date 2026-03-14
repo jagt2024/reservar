22bc8e97feaedead2c7d4c51bdc09da922f824e7
jagt2024 <josea_garciat@hotmail.com>
2026-03-14 11:45:02 -0500

    Add files via upload

--- jjgt_venpermuta/app.py
+++ jjgt_venpermuta/app.py
@@ -1737,11 +1737,11 @@ def page_publish():
         with vc1:
             pub_tipo  = st.selectbox("Tipo", ["Venta","Permuta","Venta y Permuta"], key="pf_tipo")
             pub_marca = st.selectbox("Marca",
-                ["Toyota","Chevrolet","Renault","Mazda","Hyundai","Kia","Ford", "Hamer", "Audi",
+                ["Toyota","Chevrolet","Renault","Mazda","Hyundai","Kia","Ford", "Hummer", "Audi",
                  "Volkswagen","BMW","Mercedes-Benz","Nissan","Honda","Suzuki","Jeep"], key="pf_marca")
             pub_modelo = st.text_input("Modelo *", placeholder="Ej: Corolla XEI", key="pf_modelo")
             pub_anio   = st.selectbox("Año",
-                ["2025","2024","2023","2022","2021","2020","2019","2018","2017","2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003","2002","2001","2000"], key="pf_anio")
+                ["2025","2024","2023","2022","2021","2020","2019","2018","2017","2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003","2002","2001","2000","1999","1998","1997","1996","1995","1994","1993","1992","1991","1990","1989","1988","1987","1986","1985","1984","1983","1982","1981","1980"], key="pf_anio")
             pub_km     = st.number_input("Km *", 0, 500_000, step=1_000, key="pf_km")
         with vc2:
             pub_comb  = st.selectbox("Combustible", ["Gasolina","Diesel","Híbrido","Eléctrico","Gas"], key="pf_comb")
